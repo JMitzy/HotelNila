be7e35ac1d4af01ca5fbe2c8e627056a96c98a59
Juan Rodrigo <JaunRodriCan12@gmail.com>
2024-03-02 22:11:07 -0500

    Primera Subida

---
+++ src/main/java/com/grupouno/hotelnila/domain/Habitacion.java
@@ -0,0 +1,25 @@
+package com.grupouno.hotelnila.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+@Data
+@Entity
+public class Habitacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idHabitacion;
+    private String tipoHabitacion;
+    
+    private String estado;
+    private Float precio;
+
+    @ManyToMany(mappedBy = "habitacion")
+    @JsonManagedReference
+    @JsonIgnore
+    private Set<Reserva> reserva = new HashSet<>();
+}
